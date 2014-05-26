755bdd5ec8d5ff60d518f0ed478520aa5beaab05
Filip Buszko <filipbuszko@gmail.com>
2014-05-26 20:03:23 +0200

    animation improvement

--- langtons_ant/animation.py
+++ langtons_ant/animation.py
@@ -1,9 +1,14 @@
+import random
 import pyglet
 from pyage.core.inject import Inject
+import time
 
 i = 1
 ants = {}
+moreFiles = True
 first = True
+second = False
+third = False
 
 with open('../config.txt') as f:
     content = f.read()
@@ -13,22 +18,27 @@ with open('../config.txt') as f:
     xAgents = int(args[2])
 
 def next_step(dt):
-    global i
-    with open('../positions/langtons_ant_positions_' + str(i).zfill(5)+'.txt') as f:
-        content = f.read().splitlines()
-        for line in content:
-            args = line.split(' ')
-            agentNr = int(args[0].split('.')[0])
-            xPos = int(args[1])
-            yPos = int(args[2])
-            color = int(args[3])
-            direction = int(args[4])
-            global ants
-            if agentNr in ants.keys():
-                ants[agentNr].append([xPos, yPos, color, direction])
-            else:
-                ants[agentNr] = [[xPos, yPos, color, direction]]
-    i += 1
+    global i, moreFiles
+    try:
+        if moreFiles:
+            with open('../positions/langtons_ant_positions_' + str(i).zfill(5)+'.txt') as f:
+                content = f.read().splitlines()
+                for line in content:
+                    args = line.split(' ')
+                    agentNr = int(args[0].split('.')[0])
+                    xPos = int(args[1])
+                    yPos = int(args[2])
+                    color = int(args[3])
+                    direction = int(args[4])
+                    global ants
+                    if agentNr in ants.keys():
+                        ants[agentNr].append([xPos, yPos, color, direction])
+                    else:
+                        ants[agentNr] = [[xPos, yPos, color, direction]]
+            i += 1
+    except IOError:
+        moreFiles = False
+        pass
 
 
 window = pyglet.window.Window(resizable=False)
@@ -41,27 +51,45 @@ ant_imageE = pyglet.image.load('antE.png')
 
 @window.event
 def on_draw():
-    # global first
-    # if first:
     pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
-    for k in range(0, xAgents):
-        draw_net(k*x*14+k*2, 0, x, y)
-        # first = False
+    draw_background()
     for ant in ants:
         # print ant
-        for i in range(0, len(ants[ant])-1):
-        # for position in ants[ant]:
-            set_color(ant, ants[ant][i][2])
-            offset = (ants[ant][i][0]/x)*2
-            pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,('v2i', (offset+ants[ant][i][0]*14,0+ants[ant][i][1]*14,offset+ants[ant][i][0]*14,13+ants[ant][i][1]*14,offset+13+ants[ant][i][0]*14,13+ants[ant][i][1]*14,offset+13+ants[ant][i][0]*14,0+ants[ant][i][1]*14)))
-        position = ants[ant][len(ants[ant])-1]
-        set_color(ant, position[2])
-        offset = (position[0]/x)*2
-        draw_rotated_ant(position[3]).blit(position[0]*14+offset, position[1]*14)
+        if len(ants[ant]) > 2:
+            for i in range(len(ants[ant])-3, len(ants[ant])-1):
+            # for position in ants[ant]:
+                set_color(ant, ants[ant][i][2])
+                offset = (ants[ant][i][0]/x)*2
+                pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,('v2i', (offset+ants[ant][i][0]*14,0+ants[ant][i][1]*14,offset+ants[ant][i][0]*14,13+ants[ant][i][1]*14,offset+13+ants[ant][i][0]*14,13+ants[ant][i][1]*14,offset+13+ants[ant][i][0]*14,0+ants[ant][i][1]*14)))
+            position = ants[ant][len(ants[ant])-1]
+            set_color(ant, position[2])
+            offset = (position[0]/x)*2
+            draw_rotated_ant(position[3]).blit(position[0]*14+offset, position[1]*14)
 
     # image.blit(0,0)
 
+def draw_background():
+    global first, second, third, x, y
+    if third:
+        pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
+        for k in range(0, xAgents):
+            draw_net(k*x*14+k*2, 0, x, y)
+        third = False
+    if second:
+        pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
+        for k in range(0, xAgents):
+            draw_net(k*x*14+k*2, 0, x, y)
+        second = False
+        third = True
+    if first:
+        pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
+        for k in range(0, xAgents):
+            draw_net(k*x*14+k*2, 0, x, y)
+        first = False
+        second = True
+
 def draw_net(x, y, x_dim, y_dim):
+    print x
     pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
     for i in range(0, x_dim):
         for j in range(0, y_dim):
@@ -69,14 +97,11 @@ def draw_net(x, y, x_dim, y_dim):
 
 def set_color(color, on):
     if(on == 0):
-        if(color == 1):
-            pyglet.gl.glColor4f(1.0,0,0,1.0)
-        elif(color == 2):
-            pyglet.gl.glColor4f(0,1.0,0,1.0)
-        elif(color == 4):
-            pyglet.gl.glColor4f(0,0,1.0,1.0)
-        elif(color == 5):
-            pyglet.gl.glColor4f(0.5,1.0,0,1.0)
+        random.seed(color)
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        pyglet.gl.glColor4f(r,g,b,1.0)
     else:
         pyglet.gl.glColor4f(1.0,1.0,1.0,1.0)
 
@@ -88,5 +113,5 @@ def draw_rotated_ant(direction):
         3:ant_imageN
     }[direction]
 
-pyglet.clock.schedule_interval(next_step, 1/600.0)
+pyglet.clock.schedule_interval(next_step, 1/100.0)
 pyglet.app.run()
